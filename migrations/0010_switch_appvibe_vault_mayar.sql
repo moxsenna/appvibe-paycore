41f6542db4874a39254f4d64c7d3a6911329c227
moxsenna <moxsenna@gmail.com>
2026-07-01 07:32:39 +0700

    chore: switch appvibe_vault payment provider to mayar

---
+++ migrations/0010_switch_appvibe_vault_mayar.sql
@@ -0,0 +1,13 @@
+-- Up
+UPDATE apps
+SET
+  default_merchant_profile_id = 'mp_mayar_main',
+  updated_at = (unixepoch() * 1000)
+WHERE id = 'app_appvibe_vault';
+
+-- Down
+UPDATE apps
+SET
+  default_merchant_profile_id = 'mp_appvibe_default',
+  updated_at = (unixepoch() * 1000)
+WHERE id = 'app_appvibe_vault';
